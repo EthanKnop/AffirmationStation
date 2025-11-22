b808e397996ee2db784921d5ae94db12017177e1
Ethan Knop <ethanknop@Ethans-Mac-mini.local>
2025-11-22 13:58:39 -0600

    modify routing module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { RecordingGroupManager } from './recording-group-manager/recording-group-manager';
 import { AffirmationHome } from './affirmation-home/affirmation-home';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'affirmation-home', pathMatch: 'full' },
     { path: 'affirmation-home', component: AffirmationHome },
     { path: 'affirmation-home/groups/:id', component: RecordingGroupManager }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
